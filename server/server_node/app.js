7ef63a4cce30d584d7b2747f5c4732f0c8a60932
kanav dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-10-22 19:30:34 +0530

    multi

--- server/server_node/app.js
+++ server/server_node/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/connectDB');
+const dotenv = require('dotenv');
+const email = require('./models/email')
 const app = express();
-
+dotenv.config();
 
 let corsOptions = {
     origin: ['http://127.0.0.1:5173','http://localhost:5173'], 
@@ -9,12 +12,12 @@ let corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
 
-
-app.options('*', cors(corsOptions));
+const endpoints = ()=>{
+    app.options('*', cors(corsOptions));
 
 app.get('/', (req, res) => {
-    console.log("Hello world");
     return res.status(200).json({ hello: 123 });
 });
 
@@ -29,7 +32,38 @@ app.get('/del-cookie', (req, res) => {
     res.setHeader('Set-Cookie', 'myCookie=; HttpOnly; Max-Age=0; Path=/');
     res.send('Cookie value deleted');
 });
-
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`);
+app.post('/test', async (req, res) => {
+    const { url, status } = req.body;
+    if (!url || !status) {
+        return res.status(400).send('Missing required fields: url or status');
+    }
+    try {
+        const exists= await email.findOne({url});
+        if(exists) return res.status(403).send('Already exists')
+        else{
+            const email1 = new email({ url, status });
+            await email1.save();
+            res.status(200).send('Data Stored Successfully');
+    }
+     
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
 });
+}
+const start = async ()=>{
+    try{
+        await connectDB();
+        endpoints();
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server is running on  http://localhost:${process.env.PORT}`);
+        });
+    }
+    catch(err){
+        console.error(err);
+    }
+   
+
+}
+start();
+
